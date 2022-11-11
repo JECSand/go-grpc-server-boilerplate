68ec301629ab1aed5f00ac61bcefd4014b274421
jecsand <jecsand@pm.me>
2022-11-10 23:06:37 -0600

    Added JWT interceptors for auth

--- server/auth_interceptor.go
+++ server/auth_interceptor.go
@@ -5,6 +5,9 @@ import (
 	"github.com/JECSand/go-grpc-server-boilerplate/services"
 	"github.com/JECSand/go-grpc-server-boilerplate/utilities"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
 	"log"
 )
 
@@ -29,9 +32,10 @@ func (i *AuthInterceptor) Unary() grpc.UnaryServerInterceptor {
 		handler grpc.UnaryHandler,
 	) (interface{}, error) {
 		log.Println("--> unary interceptor: ", info.FullMethod)
-
-		// TODO: implement authorization
-
+		err := i.authorize(ctx, info.FullMethod)
+		if err != nil {
+			return nil, err
+		}
 		return handler(ctx, req)
 	}
 }
@@ -45,9 +49,43 @@ func (i *AuthInterceptor) Stream() grpc.StreamServerInterceptor {
 		handler grpc.StreamHandler,
 	) error {
 		log.Println("--> stream interceptor: ", info.FullMethod)
-
-		// TODO: implement authorization
-
+		err := i.authorize(stream.Context(), info.FullMethod)
+		if err != nil {
+			return err
+		}
 		return handler(srv, stream)
 	}
+}
+
+func (i *AuthInterceptor) authorize(ctx context.Context, method string) error {
+	roleMap, ok := i.accessibleRoles[method]
+	if !ok {
+		return nil // unprotected endpoint
+	}
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		return status.Errorf(codes.Unauthenticated, "metadata is not provided")
+	}
+	values := md["authorization"]
+	if len(values) == 0 {
+		return status.Errorf(codes.Unauthenticated, "authorization token is not provided")
+	}
+	accessToken := values[0]
+	var authorized bool
+	var err error
+	switch roleMap[0] {
+	case "Root":
+		authorized, err = i.tokenService.RootAdminTokenVerifyMiddleWare(accessToken)
+	case "Admin":
+		authorized, err = i.tokenService.AdminTokenVerifyMiddleWare(accessToken)
+	case "Member":
+		authorized, err = i.tokenService.MemberTokenVerifyMiddleWare(accessToken)
+	}
+	if err != nil {
+		return status.Errorf(codes.Unauthenticated, "access token is invalid: %v", err)
+	}
+	if authorized {
+		return nil
+	}
+	return status.Error(codes.PermissionDenied, "no permission to access this RPC")
 }
